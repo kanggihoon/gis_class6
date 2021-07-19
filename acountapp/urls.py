2524f9d506ba189034d764e13dc768b22534259d
kanggihoon <65819019+kanggihoon@users.noreply.github.com>
2021-07-19 12:46:31 +0900

    7/19 class6 commit3

--- acountapp/urls.py
+++ acountapp/urls.py
@@ -16,8 +16,6 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='create'),
 
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
-
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
-
     path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 ]
